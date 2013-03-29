25f569debfbf11855432737d6c97f826c0dd7c24
Negative <gianni.mi@gmail.com>
2013-03-29 11:03:48 +0100

    optimize kss state generator

--- app/assets/javascripts/style_guide/pseudo-styles.js
+++ app/assets/javascripts/style_guide/pseudo-styles.js
@@ -5,6 +5,9 @@
       var idx, idxs, pseudos, replaceRule, rule, stylesheet, _i, _len, _len2, _ref, _ref2;
       pseudos = /(\:hover|\:disabled|\:active|\:visited|\:focus)/g;
       try {
+        var replaceRule = function(matched, stuff) {
+          return ".pseudo-class-" + matched.replace(':', '');
+        };
         _ref = document.styleSheets;
         for (_i = 0, _len = _ref.length; _i < _len; _i++) {
           stylesheet = _ref[_i];
@@ -13,9 +16,6 @@
           for (idx = 0, _len2 = _ref2.length; idx < _len2; idx++) {
             rule = _ref2[idx];
             if ((rule.type === CSSRule.STYLE_RULE) && pseudos.test(rule.selectorText)) {
-              replaceRule = function(matched, stuff) {
-                return ".pseudo-class-" + matched.replace(':', '');
-              };
               this.insertRule(rule.cssText.replace(pseudos, replaceRule));
             }
           }
@@ -23,16 +23,18 @@
       } catch (_e) {}
     }
     KssStateGenerator.prototype.insertRule = function(rule) {
-      var headEl, styleEl;
-      headEl = document.getElementsByTagName('head')[0];
-      styleEl = document.createElement('style');
-      styleEl.type = 'text/css';
-      if (styleEl.styleSheet) {
-        styleEl.styleSheet.cssText = rule;
-      } else {
-        styleEl.appendChild(document.createTextNode(rule));
+      if (!this.styleEl) {
+        this.headEl = document.getElementsByTagName('head')[0];
+        this.styleEl = document.createElement('style');
+        this.styleEl.type = 'text/css';
       }
-      return headEl.appendChild(styleEl);
+
+      if (this.styleEl.styleSheet) {
+        this.styleEl.styleSheet.cssText += rule;
+      } else {
+        this.styleEl.appendChild(document.createTextNode(rule));
+      }
+      return this.headEl.appendChild(this.styleEl);
     };
     return KssStateGenerator;
   })();
